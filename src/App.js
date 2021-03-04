28fc54e11ba3371c42acda12146441827fa9c17d
Sujan Budhathoki <sujanbudhathoki123@gmail.com>
2021-03-04 07:12:59 +0545

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const  App = () => {
+  return (
+    <div className="App">
+      <h2>Hello world</h2>
+      <p> Creating environment</p>
+    </div>
+  );
+}
+
+export default App;
